382416cdba793e9769cc2c86b10c3a34949fb5fb
Liam <liamjnell@gmail.com>
2025-11-26 10:36:02 -0700

    event pdf customization

--- PDF_TEMPLATES/event-ticket.php
+++ PDF_TEMPLATES/event-ticket.php
@@ -13,19 +13,19 @@
  */
 
 /* Prevent direct access to the template (always good to include this) */
-if ( ! class_exists( 'GFForms' ) ) {
+if (!class_exists('GFForms')) {
     return;
 }
 
 /**
  * All Gravity PDF v4/v5/v6 templates have access to the following variables:
  *
- * @var array  $form      The current Gravity Forms array
- * @var array  $entry     The raw entry data
- * @var array  $form_data The processed entry data stored in an array
- * @var object $settings  The current PDF configuration
- * @var array  $fields    An array of Gravity Forms fields which can be accessed with their ID number
- * @var array  $config    The initialised template config class – eg. /config/zadani.php
+ * @var array $form The current Gravity Forms array
+ * @var array $entry The raw entry data
+ * @var array $form_data The processed entry data stored in an array
+ * @var object $settings The current PDF configuration
+ * @var array $fields An array of Gravity Forms fields which can be accessed with their ID number
+ * @var array $config The initialised template config class – eg. /config/zadani.php
  */
 
 ?>
@@ -53,11 +53,15 @@ if ( ! class_exists( 'GFForms' ) ) {
     </div>
     <div class="field-row">
         <span class="label">Add-Ons:</span>
-        <ul>
-            <?php foreach($form_data['field'][8] as $addon) : ?>
-                <li><php echo esc_html($addon); ?></li>
-            <?php endforeach; ?>
-        </ul>
+        <?php if (!empty($form_data['field'][8])) : ?>
+            <ul>
+                <?php foreach ($form_data['field'][8] as $addon) : ?>
+                    <li><?php echo esc_html($addon); ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else : ?>
+            <p>No add-ons</p>
+        <?php endif; ?>
 
     </div>
 </div>
